0d282a64cceb5e77bc2518a8dd9da35e214b3f90
Yosef Bedaso <yosefmaru@gmail.com>
2017-11-22 14:26:35 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 # Set Win 32bit Python 2 file as the default source.
-sys.path.append(os.path.join(os.path.dirname(__name__), '../SWIG'))
+sys.path.append(os.path.join(os.path.dirname(__name__), '../SWIG/python'))
 
 # -- General configuration ------------------------------------------------
 
