59236039fc0429f213ef94b48155b734f7822832
hsauro <hsauro@gmail.com>
2017-12-15 19:53:57 -0800

    test file is now better but needs more work

--- SWIG/test/tests/testLibStructural.py
+++ SWIG/test/tests/testLibStructural.py
@@ -161,79 +161,41 @@ except Exception as e:
 # Builtin test
   
 ls.test()
-##%%
-#r = te.loada('''
-#    
-#    // Reactions:
-#    J1: ES -> E  + S1; v;
-#    J2: S1 -> S2 ; v;
-#    J3: S2 + E -> ES; v;
-#
-#    
-#    // Variable initializations:
-#        v = 0;
-#    
-#    // Species initializations:
-#    S1 = 100; E = 20;
-#    
-#''')
-#
-#sbmlstr = r.getSBML()
-#ls = structural.LibStructural()
-#ls.loadSBMLFromString(sbmlstr)
-#print(ls.getSummary())
-##%%
-#
-#model_path = pkg_resources.resource_filename('structural','test_models/BMID000000101155.xml')
-#ls = structural.LibStructural()
-#ls.loadSBMLFromFile(model_path)
-#print(ls.getSummary())
-#
-##%%
-##print ls.analyzeWithFullyPivotedLU()
-#print ls.analyzeWithFullyPivotedLUwithTests()
-#print ls.analyzeWithLU()
-#print ls.analyzeWithLUandRunTests()
-#print ls.analyzeWithQR()
-##%%
-#import structural
-#ls = structural.LibStructural()
-##%%
-#
+
+# Test Conservation Analysis
+r = te.loada('''
+    
+    // Reactions:
+    J1: ES -> E  + S1; v;
+    J2: S1 -> S2 ; v;
+    J3: S2 + E -> ES; v; 
+    v = 0;
+    
+    // Species initializations:
+    S1 = 100; E = 20;
+    
+''')
+
+sbmlstr = r.getSBML()
+ls = structural.LibStructural()
+ls.loadSBMLFromString(sbmlstr)
+print(ls.getSummary())
+
+
 #
 #print ls.rref(ls.getNICMatrix())
 #print ls.rref_FB(ls.getNICMatrix()) # change to rrefReordered
 #print ls.setTolerance(1e-7)
 #print ls.validateStructuralMatrices()
 #
-#
-##%%
-#
-#import structural
-#
-#ls = structural.LibStructural()
-#matrix = [[  1, -1, -1], [  0, -1,  1], [  0,  1, -1]] # matrix can be a numpy 2d array
-#ls.loadStoichiometryMatrix(matrix)
-##ls.loadSBMLFromString(sbmlstr)
-#print ls.getStoichiometryMatrix()
-#print ls.getSpeciesIds(), "Sp Ids"
-#
-#print ls.getReactionsIds(), "Rxn Ids"
-#
-#
-#print('\n\n')
-## load Names
-#ls.loadSpeciesIdsWithValues (['a', 'b', 'c'], [0, 0, 0])
-#ls.loadReactionIdsWithValues (['F1', 'F2', 'F3'],[0, 0, 0])
-#
-#ls.analyzeWithQR()
-#
-#print ls.getSpeciesIds(), "Sp Ids"
-#print ls.getReactionsIds(), "Rxn Ids"
-#
-##%%
-#print(ls.validateStructuralMatrices()) # Prints out if the model is passed some interna structural validation tests.
-#
-#  # To see whats tests was performed call ls.test
-#tests = ls.getTestDetails()
-#print(tests)
+
+
+print "Test Manual Loading of a Model:"
+matrix = [[  1, -1, -1], [  0, -1,  1], [  0,  1, -1]] # matrix can be a numpy 2d array
+ls.loadStoichiometryMatrix(matrix)
+print ls.getStoichiometryMatrix()
+print ls.getSpeciesIds(), "Sp Ids"
+
+print ls.getReactionsIds(), "Rxn Ids"
+print(ls.getSummary())
+print(ls.validateStructuralMatrices()) # Prints out if the model is passed some interna structural validation tests.
