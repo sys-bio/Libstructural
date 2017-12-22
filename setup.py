59435ef3448a7bff91ced52a37a4594a7477f463
Yosef <yosefmaru@gmail.com>
2017-12-22 10:25:45 -0700

    change on setup.py

--- setup.py
+++ setup.py
@@ -82,14 +82,14 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/yosefmaru/Libstructural',
+    url='https://github.com/sys-bio/Libstructural',
 
     # Author details
     author='Herbert M. Sauro and Frank T. Bergmann ',
     author_email='hsauro@uw.edu',
 
     # Choose your license
-    license='DSW',
+    license='BSD',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -103,7 +103,7 @@ setup(
         'Intended Audience :: Developers',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: BSD License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
